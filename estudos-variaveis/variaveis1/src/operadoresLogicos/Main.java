976c269baccad0210e98a451a0c9c5c7121b39ad
jonatas.almeida <jonatas.almeida@globalhitss.com.br>
2023-09-17 20:17:33 -0300

    operadores relacionais

---
+++ estudos-variaveis/variaveis1/src/operadoresLogicos/Main.java
@@ -0,0 +1,19 @@
+package operadoresLogicos;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        
+        boolean verdadeiro = 5 > 2 && (2 + 2 == 4);
+        System.out.println(verdadeiro);
+        
+        boolean verdadeiroOuFalso = (1 - 1) == 0 && false;
+        System.out.println(verdadeiroOuFalso);
+        
+        boolean falso = false;
+        System.out.println(falso);
+
+
+
+
+    }
+}
